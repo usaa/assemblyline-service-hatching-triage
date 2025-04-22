2f70142e387daabaf101a8ae7fec21b064269b9f
Eric L <33640251+ed4wg@users.noreply.github.com>
2025-04-22 02:20:13 +0000

    add AL4 expected heuristic ids to service_manifest

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+## [4.6.0.stable1] - 2025-4-25
+
+### Changed
+
+- add AL4 expected heuristic ids to service_manifest
+
 ## [4.5.0.stable34] - 2024-11-22
 
 ### Changed
--- tests/test_hatching_result_HatchingResult__build_process_section.py
+++ tests/test_hatching_result_HatchingResult__build_process_section.py
@@ -84,7 +84,7 @@ def test_build_process_section(
             "safelisted": False,
         }
     ]
-    assert section.heuristic is None
+    assert section.heuristic._heur_id is 57
     # log.warning(section.tags)
     assert section.tags == {
         "dynamic.processtree_id": [
